b05e0f43ef1c2a1c30d01eabe51643e66e999877
DonavanJones <donavancjones@gmail.com>
2024-04-25 15:32:13 -0400

    Article Batch 2

--- app.vue
+++ app.vue
@@ -44,6 +44,19 @@ useSchemaOrg([
 </template>
 
 <style>
+.iframe-container {
+  position: relative;
+  width: 100%;
+  padding-top: 56.25%; /* 16:9 aspect ratio (divide 9 by 16 = 0.5625) */
+}
+
+.iframe-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 .rotate-enter-active,
 .rotate-leave-active {
   transition: all 0.4s;
